2b50419c7172ba3c8f7cf4f83d0eb830f80cafb7
Vladislav Chernyshev <vladchern9@gmail.com>
2026-02-10 19:58:24 +1000

    Second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from Docker!")
+        self.wfile.write(b"Hello from Docker! It`s working!")
 
 if __name__ == "__main__":
     HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
